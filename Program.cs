095dd17859dbb33bbdacf37aed80f8d9616e0810
Erick Rick <erickcredjes@gmail.com>
2024-04-20 11:26:06 -0300

    Inicinado processo de avaliação das bandas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿// Screen Sound
 string mensageDeBoasVindas = "Boas vindas ao Screen Sound";
-List<string> listaDasBandas = new List<string>
-{ "U2", "LinkPark", "Mamonas Assasinas", "Code Play" };
+//List<string> listaDasBandas = new List<string>
+//{ "U2", "LinkPark", "Mamonas Assasinas", "Code Play" };
+
+Dictionary<string, List<int>> listaDasBandas = new Dictionary<string, List<int>>();
+listaDasBandas.Add("Link Park", new List<int> { 1, 2, 10 });
+listaDasBandas.Add("The Beatles", new List<int>());
 
 void ExibirLogo()
 {
@@ -39,13 +43,14 @@ void ExibirOpcoesDoMenu()
         MostrarbandasRegistradas();
         break;
       case 3:
-        Console.WriteLine($"opção escolhida: {opcaoEscolhidaNumerica}");
+        AvaliacaoUmaBanda();
         break;
       case 4:
         Console.WriteLine($"opção escolhida: {opcaoEscolhidaNumerica}");
         break;
       case -1:
         Console.WriteLine($"{opcaoEscolhidaNumerica} saindo do sitema");
+        Environment.Exit(0);
         break;
       default:
         Console.WriteLine($"Opção ({opcaoEscolhidaNumerica}) invalida");
@@ -65,21 +70,31 @@ void ExibirOpcoesDoMenu()
 
 }
 
-void RegistrarBanda()
+void RegistrarBanda(string? nomeBanda = null)
 {
   Console.Clear();
   ExibirTituloDaOpcao("Registro de bandas");
 
-  Console.Write("Digite o nome da banda que deseja: ");
-  string nomeDaBanda = Console.ReadLine()!;
+  if (nomeBanda is null)
+  {
+    Console.Write("Digite o nome da banda que deseja: ");
+    string nomeDaBanda = Console.ReadLine()!;
 
-  listaDasBandas.Add(nomeDaBanda);
+    listaDasBandas.Add(nomeDaBanda, new List<int>());
 
-  Console.WriteLine($"\r\nA banda ({nomeDaBanda}) foi registrada com sucesso");
-  Thread.Sleep(2000);
+    Console.WriteLine($"\r\nA banda ({nomeDaBanda}) foi registrada com sucesso");
+    Thread.Sleep(2000);
 
-  Console.Clear();
-  ExibirOpcoesDoMenu();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+  }
+  else
+  {
+    listaDasBandas.Add(nomeBanda, new List<int>());
+
+    Console.WriteLine($"\r\nA banda ({nomeBanda}) foi registrada com sucesso");
+  }
+
 }
 
 void MostrarbandasRegistradas()
@@ -95,8 +110,8 @@ void MostrarbandasRegistradas()
     ExibirOpcoesDoMenu();
   }
 
-  foreach (string nomeDaBanda in listaDasBandas)
-    Console.WriteLine($"Banda: {nomeDaBanda}");
+  foreach (string nomeDaBanda in listaDasBandas.Keys)
+      Console.WriteLine($"Banda: {nomeDaBanda}");
 
   Console.WriteLine("\r\nDigite uma tecla para voltar a menu principal");
 
@@ -112,6 +127,58 @@ void ExibirTituloDaOpcao(string titulo)
   Console.WriteLine(asteriscos);
   Console.WriteLine(titulo);
   Console.WriteLine($"{asteriscos}\r\n");
+}
+
+void AvaliacaoUmaBanda()
+{
+  Console.Clear();
+  ExibirTituloDaOpcao("Avaliar uma banda");
+
+  Console.Write("Digite o nome da banda que deseja avaliar: ");
+  string nomeDaBanda = Console.ReadLine()!;
+  bool bandaExiste = listaDasBandas.ContainsKey(nomeDaBanda);
+
+  if (bandaExiste)
+  {
+
+  }
+  else
+  {
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar uma banda");
+
+    Console.WriteLine("Banda informada não cadastrada.\r\nO que deseja fazer?\r\n");
+    Console.WriteLine("1 - cadastrar essa banda");
+    Console.WriteLine("2 - Voltar para o Menu principal");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    while (opcaoEscolhidaNumerica != 2)
+      switch (opcaoEscolhidaNumerica)
+      {
+        case 1:
+          RegistrarBanda(nomeDaBanda);
+          Thread.Sleep(4000);
+          Console.Clear();
+          AvaliacaoUmaBanda();
+          break;
+        case 2:
+          Console.Clear();
+          ExibirOpcoesDoMenu();
+          break;
+        default:
+          Console.WriteLine("Porfavor escolha uma opção valida");
+          Console.WriteLine("1 - cadastrar essa banda");
+          Console.WriteLine("2 - Voltar para o Menu principal");
+
+          opcaoEscolhida = Console.ReadLine()!;
+          opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+          break;
+      }
+    
+  }
+
+
 }
 
 ExibirOpcoesDoMenu();
